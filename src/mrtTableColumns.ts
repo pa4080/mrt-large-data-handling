12fbb2f5c3a824b33012eec5829a6517b1296dd9
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2024-07-14 15:30:59 +0000

    feat: add a Mimimal Example of useRowSelectionWorkaround() hook usage

---
+++ src/mrtTableColumns.ts
@@ -0,0 +1,137 @@
+import { MRT_ColumnDef } from "material-react-table";
+import { TableData } from "./makeData";
+
+export const tableColumns: MRT_ColumnDef<TableData>[] = [
+	{
+		header: "First Name",
+		accessorKey: "firstName",
+	},
+	{
+		header: "Last Name",
+		accessorKey: "lastName",
+	},
+	{
+		header: "Age",
+		accessorKey: "age",
+	},
+	{
+		header: "Gender",
+		accessorKey: "gender",
+	},
+	{
+		header: "State",
+		accessorKey: "state",
+	},
+	{
+		header: "Salary",
+		accessorKey: "salary",
+	},
+	{
+		header: "Email",
+		accessorKey: "email",
+	},
+	{
+		header: "Phone Number",
+		accessorKey: "phoneNumber",
+	},
+	{
+		header: "Address",
+		accessorKey: "address",
+	},
+	{
+		header: "Occupation",
+		accessorKey: "occupation",
+	},
+	{
+		header: "Hobbies",
+		accessorKey: "hobbies",
+	},
+	{
+		header: "Education",
+		accessorKey: "education",
+	},
+	{
+		header: "Favorite Color",
+		accessorKey: "favoriteColor",
+	},
+	{
+		header: "Favorite Food",
+		accessorKey: "favoriteFood",
+	},
+	{
+		header: "Favorite Movie",
+		accessorKey: "favoriteMovie",
+	},
+	{
+		header: "Favorite Book",
+		accessorKey: "favoriteBook",
+	},
+	{
+		header: "Favorite Song",
+		accessorKey: "favoriteSong",
+	},
+	{
+		header: "Favorite Vacation Spot",
+		accessorKey: "favoriteVacationSpot",
+	},
+	{
+		header: "Favorite Pet",
+		accessorKey: "favoritePet",
+	},
+	{
+		header: "Favorite Sport",
+		accessorKey: "favoriteSport",
+	},
+	{
+		header: "Favorite Number",
+		accessorKey: "favoriteNumber",
+	},
+	{
+		header: "Favorite Quote",
+		accessorKey: "favoriteQuote",
+	},
+	{
+		header: "Favorite Memory",
+		accessorKey: "favoriteMemory",
+	},
+	{
+		header: "Favorite Inspirational Quote",
+		accessorKey: "favoriteInspirationalQuote",
+	},
+	{
+		header: "Favorite Life Goal",
+		accessorKey: "favoriteLifeGoal",
+	},
+	{
+		header: "Favorite Dream Vacation",
+		accessorKey: "favoriteDreamVacation",
+	},
+	{
+		header: "Favorite Dream Car",
+		accessorKey: "favoriteDreamCar",
+	},
+	{
+		header: "Favorite Dream Home",
+		accessorKey: "favoriteDreamHome",
+	},
+	{
+		header: "Favorite Dream Partner",
+		accessorKey: "favoriteDreamPartner",
+	},
+	{
+		header: "Favorite Dream Holiday",
+		accessorKey: "favoriteDreamHoliday",
+	},
+	{
+		header: "Favorite Dream Job",
+		accessorKey: "favoriteDreamJob",
+	},
+	{
+		header: "Favorite Dream Travel Style",
+		accessorKey: "favoriteDreamTravelStyle",
+	},
+	{
+		header: "Favorite Dream Retirement",
+		accessorKey: "favoriteDreamRetirement",
+	},
+];
